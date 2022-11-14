7c487b17cacfd48d30597ebc041266bdc267adf4
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-11-14 10:09:28 +0100

    submodule update

--- test/generic.cc
+++ test/generic.cc
@@ -8,7 +8,8 @@
 int
 main()
 {
-  graph<vertex<edge<unsigned, SU>>> g(2);
+  using edge_type = edge<unsigned, SU>;
+  graph<vertex<edge_type>> g(2);
   auto &v1 = add_vertex(g, "v0");
   auto &v2 = add_vertex(g, "v1");
   add_edge(v1, v2, 1, {{0, 5}});
@@ -19,7 +20,7 @@ main()
   // The null edge that is not part of the graph.
   edge<unsigned, SU> null_edge(v1, v1, 0, {});
 
-  generic_permanent<label_type> P(num_vertexes(g));
-  generic_tentative<label_type> T(num_vertexes(g));
+  generic_permanent<label_type, edge_type> P(num_vertexes(g));
+  generic_tentative<label_type, edge_type> T(num_vertexes(g));
   // dijkstra(label_robe(initial, null_edge), P, T);
 }
