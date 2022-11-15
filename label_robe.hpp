3eb1563f0a144d65838ceace879b4c86e27cd542
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-11-15 08:33:58 +0100

    added traits to the label robe

--- label_robe.hpp
+++ label_robe.hpp
@@ -1,6 +1,8 @@
 #ifndef LABEL_ROBE_HPP
 #define LABEL_ROBE_HPP
 
+#include "graph_interface.hpp"
+
 #include <utility>
 
 template <typename Edge, typename Label>
@@ -22,5 +24,17 @@ get_target(const label_robe<Label, Edge> &r)
 {
   return get_target(r.m_edge);
 }
+
+template <typename Edge, typename Label>
+struct edge_traits<label_robe<Edge, Label>>
+{
+  using type = Edge;
+};
+
+template <typename Edge, typename Label>
+struct weight_traits<label_robe<Edge, Label>>
+{
+  using type = Weight<Label>;
+};
 
 #endif // LABEL_ROBE_HPP
