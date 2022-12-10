30df00cbe3be37ce02e2df7f2fc4c5bb5d6e586d
Ireneusz Szcześniak <irek.szczesniak@gmail.com>
2022-12-10 14:37:01 +0100

    added a test for a constrained search

--- test/standard.cc
+++ test/standard.cc
@@ -1,5 +1,6 @@
 #include "dijkstra.hpp"
 #include "standard_label_creator.hpp"
+#include "standard_constrained_label_creator.hpp"
 #include "standard_permanent.hpp"
 #include "standard_tentative.hpp"
 #include "standard_tracer.hpp"
@@ -24,25 +25,34 @@ main()
   graph_type g(2);
   auto &v1 = add_vertex(g, "v0");
   auto &v2 = add_vertex(g, "v1");
-  add_edge(v1, v2, 1);
+  add_edge(v1, v2, 10);
 
   // The null edge that is not part of the graph.
   edge_type null_edge(v1, v1, 0);
   // The label type, and the initial label.
-  using robe_type = label_robe<edge_type, weight<int>>;
+  using robe_type = label_robe<edge_type, weight<unsigned>>;
   robe_type initial(null_edge, 0);
 
-  standard_permanent<robe_type> P(num_vertexes(g));
-  standard_tentative<robe_type> T(num_vertexes(g));
-  dijkstra(initial, P, T, standard_label_creator());
+  // There is no path of weight 5 or less.
+  {
+    standard_permanent<robe_type> P(num_vertexes(g));
+    standard_tentative<robe_type> T(num_vertexes(g));
+    dijkstra(initial, P, T, standard_constrained_label_creator(5u));
+    auto op = trace(initial, v2, standard_tracer(P));
+    assert(!op);
+  }
 
-  // Get and return the path.
-  auto op = trace(initial, v2, standard_tracer(P));
+  {
+    standard_permanent<robe_type> P(num_vertexes(g));
+    standard_tentative<robe_type> T(num_vertexes(g));
+    dijkstra(initial, P, T, standard_label_creator());
+    auto op = trace(initial, v2, standard_tracer(P));
 
-  if (op)
-    {
-      const auto &p = op.value();
-      for(const auto &l: p)
-        std::cout << l << std::endl;
-    }
+    if (op)
+      {
+        const auto &p = op.value();
+        for(const auto &l: p)
+          std::cout << l << std::endl;
+      }
+  }
 }
